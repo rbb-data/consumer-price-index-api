4ef057fb38101aca001529a05e9f77f7dd9af871
sophiamersmann <sophia.mersmann1@gmail.com>
2022-07-12 16:29:25 +0200

    update database on post requests

--- index.js
+++ index.js
@@ -12,56 +12,128 @@ const client = new SecretManagerServiceClient();
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.consumerPriceIndexAPI = async (req, res) => {
-  // Handle CORS
+  // handle CORS
   res.set('Access-Control-Allow-Origin', '*');
-
   if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
     res.set('Access-Control-Allow-Methods', 'GET');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.set('Access-Control-Max-Age', '3600');
-    res.status(204).send('');
-  } else {
-    const createUnixSocketPool = async (config) => {
-      const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+    res.status(204).send('').end();
+  }
 
-      // establish a connection to the database
-      return mysql.createPool({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
-        ...config,
-      });
+  const accessSecret = async (name) => {
+    const [secret] = await client.accessSecretVersion({ name });
+    return secret.payload.data.toString('utf8');
+  };
+
+  const createUnixSocketPool = async (config) => {
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
+    // establish a connection to the database
+    return mysql.createPool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
+      ...config,
+    });
+  };
+
+  const createPool = async () => {
+    const config = {
+      connectionLimit: 1,
+      connectTimeout: 10000,
+      acquireTimeout: 10000,
+      waitForConnections: true,
+      queueLimit: 0,
     };
 
-    const createPool = async () => {
-      const config = {
-        connectionLimit: 1,
-        connectTimeout: 10000,
-        acquireTimeout: 10000,
-        waitForConnections: true,
-        queueLimit: 0,
-      };
+    // access db password and store it in an environment variable
+    process.env.DB_PASS = await accessSecret(
+      process.env.CLOUD_SQL_CREDENTIALS_SECRET
+    );
 
-      // access secret and store it in a environment variable
-      const [secret] = await client.accessSecretVersion({
-        name: process.env.CLOUD_SQL_CREDENTIALS_SECRET,
-      });
-      process.env.DB_PASS = secret.payload.data.toString('utf8');
+    return createUnixSocketPool(config);
+  };
 
-      return createUnixSocketPool(config);
-    };
-
-    const pool = await createPool().catch((err) => {
-      throw err;
+  async function handleGET(req, res) {
+    // establish database connection
+    const pool = await createPool().catch(() => {
+      res.status(500).send("Database connection can't be established").end();
     });
 
     // example
-    const entry = await pool.query(
-      'SELECT * FROM consumer_price_index LIMIT 1'
-    );
+    const sql = 'SELECT * FROM consumer_price_index LIMIT ?';
+    const entries = await pool.query(sql, [5]);
 
-    res.status(200).json(entry[0]);
+    res.status(200).json(entries).end();
   }
+
+  async function handlePOST(req, res) {
+    const authorize = async (req) => {
+      const { authorization } = req.headers;
+
+      if (!authorization || !authorization.startsWith('Bearer')) {
+        return { ok: false, msg: 'No bearer token provided' };
+      }
+
+      const token = authorization.replace('Bearer', '').trim();
+      const secret = await accessSecret(process.env.CLOUD_API_SECRET);
+
+      if (token !== secret) return { ok: false, msg: 'Token invalid' };
+
+      return { ok: true };
+    };
+
+    const checkData = (data, columns) => {
+      if (!data) return { ok: false, msg: 'No data provided' };
+
+      const isValid = (d) => columns.every((col) => d[col] !== undefined);
+
+      if (
+        (!Array.isArray(data) && typeof data !== 'object') ||
+        (Array.isArray(data) && !data.every(isValid)) ||
+        (!Array.isArray(data) && typeof data === 'object' && !isValid(data))
+      ) {
+        return { ok: false, msg: 'Invalid data format' };
+      }
+
+      return { ok: true };
+    };
+
+    // check if request is authorized
+    const { ok: isAuthorized, msg: authMsg } = await authorize(req);
+    if (!isAuthorized) res.status(401).send(authMsg).end();
+
+    // check if the provided data is valid
+    const columns = ['item_id', 'item_name', 'year', 'month', 'value'];
+    const { body: data } = req;
+    const { ok: isValid, msg: dataMsg } = checkData(data, columns);
+    if (!isValid) res.status(400).send(dataMsg).end();
+
+    // establish connection
+    const pool = await createPool().catch(() => {
+      res.status(500).send("Database connection can't be established").end();
+    });
+
+    // insert data into database
+    try {
+      if (Array.isArray(data)) {
+        const sql = `INSERT INTO consumer_price_index (${columns}) VALUES ? ON DUPLICATE KEY UPDATE value = VALUES(value)`;
+        const values = data.map((d) => columns.map((col) => d[col]));
+        await pool.query(sql, [values]);
+      } else if (typeof data === 'object') {
+        const sql =
+          'INSERT INTO consumer_price_index SET ? ON DUPLICATE KEY UPDATE value = ?';
+        await pool.query(sql, [data, data.value]);
+      }
+    } catch (err) {
+      return res.status(500).send('Unable to insert data into table').end();
+    }
+
+    res.status(200).send('');
+  }
+
+  if (req.method === 'GET') await handleGET(req, res);
+  else if (req.method === 'POST') await handlePOST(req, res);
 };
